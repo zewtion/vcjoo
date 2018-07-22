91d679b0e4d0ffad757fd4c9e93b2e10f8c1634f
AYAN <zewtion@gmail.com>
2018-07-23 03:17:57 +0900

    -

--- src/index.js
+++ src/index.js
@@ -1,4 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import data1 from './data.json';
 
-ReactDOM.render(<p>Hello world!!!</p>, document.getElementById('root'));
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedKey : -1,
+            keyword : '',
+            danjis : data1.danjis[0],
+            searchWord : data1.searchWord
+            
+        }
+
+        this.jsonSearch = this.jsonSearch.bind(this);
+    }
+
+    jsonSearch( key ){
+        this.setState=({
+            keyword : '',
+            selectedKey : key
+        });
+
+        console.log(key + '[key]');
+    }
+    
+    render(){
+        return(
+            <div>
+                { data1.searchWord }<br/>
+                <input id='luv' type='text' placeholder='Search'/>
+                <button onClick={this.jsonSearch( 1 )}> search </button>
+                <br/>
+                <Step1 name='Hello vcjoo' />
+            </div>
+
+        );
+    }
+};
+
+export default class Step1 extends React.Component{
+    render(){
+        return(
+            <div>
+                sysout.{this.props.name}.print !!
+            </div>
+        );
+    }
+};
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
